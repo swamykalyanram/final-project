533bae201c93a55ca2a99834cf505d44041045f2
swamykalyanram <kalyan@keepworks.com>
2017-04-03 15:19:28 +0530

    added app icon and modified activity_sound.xml

--- app/src/main/java/com/example/kalyanramswamy/final_project/MainActivity.java
+++ app/src/main/java/com/example/kalyanramswamy/final_project/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import static android.Manifest.*;
 
--- app/src/main/java/com/example/kalyanramswamy/final_project/soundActivity.java
+++ app/src/main/java/com/example/kalyanramswamy/final_project/soundActivity.java
@@ -8,6 +8,8 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class soundActivity extends AppCompatActivity {
